dd851a693abc54d1678ff4221e7b72fb4fe42805
simba-fs <simba.fs@gmail.com>
2020-07-05 21:42:51 +0800

    isolate react-static

---
+++ react-static/README.md
@@ -0,0 +1,16 @@
+# react-static support
+If you want to add  react support to your project, overwrite files in this folder to the root of the project.  
+You can also add the follow script to `package.json`.  
+Don't forget to install `react-static` with npm or yarn
+
+## package.json
+```
+"static": "react-static build"
+```
+
+## install react-static
+```
+npm i -D react-static
+```
+
+More information at [react-static github repo](https://github.com/react-static/react-static)
--- src/index.js
+++ src/index.js
@@ -1,43 +1,6 @@
-// This file is for react-static, if you don't need it just use the follow commented code replaces it.
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-//
-// import App from './App.js';
-//
-// ReactDom.render(<App />, document.getElementById('root'));
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Your top level component
 import App from './App.js';
 
-// Export your top level component as JSX (for static rendering)
-export default App;
-
-// Render your app
-if (typeof document !== 'undefined') {
-	const target = document.getElementById('root');
-
-	const renderMethod = target.hasChildNodes()
-		? ReactDOM.hydrate
-		: ReactDOM.render;
-
-	const render = Comp => {
-		renderMethod(
-			<Comp />,
-			target
-		);
-	};
-
-	// Render!
-	render(App);
-
-	// Hot Module Replacement
-	if (module && module.hot) {
-		module.hot.accept('./App', () => {
-			render(App);
-		});
-	}
-}
+ReactDOM.render(<App />, document.getElementById('root'));
